5f2a2c54411a7f03f26e9c72a242e3361b4fe3c1
joseulisses065 <sulisses111@gmail.com>
2024-01-30 22:04:45 -0300

    ficture: insert, delete, select costs

--- app/Http/Controllers/CostController.php
+++ app/Http/Controllers/CostController.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CostRequest;
+use App\Models\Cost;
 use Illuminate\Http\Request;
 
 class CostController extends Controller
 {
     //
-    public function index(){
-        return view('cost.index');
+    public function index(Cost $costs){
+        $exit = 0;
+        $entry = 0;
+        foreach($costs->all() as $cost){
+            if($cost->type == 'entry'){
+                $entry+=$cost->value;
+            }else{
+                $exit+= $cost->value;
+            }
+        }
+        $balance = $entry-$exit;
+
+        return view('cost.index')->with('costs',$costs)->with('entry',$entry)->with('exit',$exit)->with('balance',$balance);
+    }
+
+    public function store(CostRequest $request){
+
+        $costs = Cost::create($request->all());
+        $costs->save();
+
+        return to_route('cost.index')->with('success','Salvo com sucesso');
+
+
+    }
+
+    public function destroy(Cost $cost){
+
+        $cost->delete();
+        return to_route('cost.index')->with('success','Removido com sucesso');
+
+
     }
 }
--- resources/views/cost/index.blade.php
+++ resources/views/cost/index.blade.php
@@ -1,4 +1,9 @@
 <x-layout title="Dashboard">
+    @isset($success)
+    <div class="alert alert-success">
+        {{ $success }}
+    </div>
+    @endisset
     <section class="text-center
     p-5">
         <h1 class="text-light fw-bold">COST CONTROLLER</h1>
@@ -8,26 +13,27 @@
         <section class="values justify-content-around d-none d-lg-flex ">
             <div class="card text-bg-light mb-3 px-5" style="min-width: 22rem;">
                 <div class="py-2 mb-0 d-flex justify-content-between">
-                    <p class="fs-4">Entry</p><i class="bi bi-box-arrow-in-down fs-4"></i></i>
+                    <p class="fs-4">Entry</p><i class="bi bi-box-arrow-in-down fs-4"></i>
                 </div>
                 <div class="card-body pt-0">
-                    <h2 class="">R$ 15.55</h2>
+                    <h2 class="">R$ {{$entry}}</h2>
                 </div>
             </div>
             <div class="card text-bg-light mb-3 px-5" style="min-width: 22rem;">
                 <div class="py-2 mb-0 d-flex justify-content-between">
-                    <p class="fs-4">Exit</p><i class="bi bi-box-arrow-in-up fs-4"></i></i>
+                    <p class="fs-4">Exit</p><i class="bi bi-box-arrow-in-up fs-4"></i>
                 </div>
                 <div class="card-body pt-0">
-                    <h2 class="">R$ 15.55</h2>
-                </div>
+                    <h2 class="">R$ {{$exit}}
+                </div>/h2>
+            </div>
             </div>
             <div class="card text-bg-light mb-3 px-5" style="min-width: 22rem;">
                 <div class="py-2 mb-0 d-flex justify-content-between fs-4">
-                    <p class="fs-5">Saldo</p><i class="bi bi-currency-dollar"></i>
+                    <p class="fs-5">Balance</p><i class="bi bi-currency-dollar"></i>
                 </div>
                 <div class="card-body pt-0">
-                    <h2 class="">R$ 15.55</h2>
+                    <h2 class="">R$ {{$balance}}</h2>
                 </div>
             </div>
         </section>
@@ -49,27 +55,27 @@
                                 <p class="fs-4">Entry</p><i class="bi bi-box-arrow-in-down fs-4"></i></i>
                             </div>
                             <div class="card-body pt-0">
-                                <h2 class="">R$ 15.55</h2>
+                                <h2 class="">R$ {{$entry}}</h2>
                             </div>
                         </div>
                     </div>
                     <div class="carousel-item px-2">
                         <div class="card text-bg-light mb-3 px-5" style="min-width: 22rem;">
                             <div class="py-2 mb-0 d-flex justify-content-between">
-                                <p class="fs-4">Entry</p><i class="bi bi-box-arrow-in-down fs-4"></i></i>
+                                <p class="fs-4">Exit</p><i class="bi bi-box-arrow-in-down fs-4"></i></i>
                             </div>
                             <div class="card-body pt-0">
-                                <h2 class="">R$ 15.55</h2>
+                                <h2 class="">R$ {{$exit}}</h2>
                             </div>
                         </div>
                     </div>
                     <div class="carousel-item px-2">
                         <div class="card text-bg-light mb-3 px-5" style="min-width: 22rem;">
                             <div class="py-2 mb-0 d-flex justify-content-between">
-                                <p class="fs-4">Entry</p><i class="bi bi-box-arrow-in-down fs-4"></i></i>
+                                <p class="fs-4">Balance</p><i class="bi bi-box-arrow-in-down fs-4"></i></i>
                             </div>
                             <div class="card-body pt-0">
-                                <h2 class="">R$ 15.55</h2>
+                                <h2 class="">R$ {{$balance}}</h2>
                             </div>
                         </div>
                     </div>
@@ -87,26 +93,30 @@
             </div>
         </section>
     </section>
-    <section class="container px-5">
-        <form action="" class=" bg-light p-2 d-flex  gap-4 justify-content-around mb-4 rounded">
+    <section class="container px-4">
+        <form action="{{route('cost.store')}}" class=" bg-light p-2 d-flex  gap-4 justify-content-around mb-4 rounded"
+            method="post">
+            @csrf
             <div class="form-group">
                 <input type="text" name="description" placeholder="Description" class="form-control">
             </div>
             <div class="form-group">
-                <input type="number" name="Value" placeholder="Value" class="form-control">
+                <input type="number" name="value" placeholder="Value" class="form-control">
             </div>
-            <div class="form-group d-flex justify-content-between ali">
-                <div class="form-check m-1">
-                    <input class="form-check-input p-1" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                    <label class="form-check-label" for="flexRadioDefault1">
-                        Exit </label>
-                </div>
-                <div class="form-check m-1">
-                    <input class="form-check-input p-1" type="radio" name="flexRadioDefault" id="flexRadioDefault2"
-                        checked>
-                    <label class="form-check-label" for="flexRadioDefault2">
-                        Entry </label>
-                </div>
+            <div class="form-group pt-2">
+                <fieldset class="d-flex justify-content-between  gap-2">
+                    <div>
+                        <input type="radio" id="Entry" name="type" value="Entry" checked />
+                        <label for="Entry">Entry</label>
+                    </div>
+
+                    <div>
+                        <input type="radio" id="Exit" name="type" value="Exit" />
+                        <label for="Exit">Exit</label>
+                    </div>
+
+                </fieldset>
+
             </div>
             <div>
                 <button class="btn btn-warning text-light bg-detail">Save</button>
@@ -115,10 +125,10 @@
 
     </section>
 
-    <section class="container data px-5 rounded">
+    <section class="container data px-4 rounded">
         <table class="table  rounded">
             <thead>
-                <tr>
+                <tr class="text-center">
                     <th scope="col">Description</th>
                     <th scope="col">Value</th>
                     <th scope="col">Tipe</th>
@@ -127,40 +137,32 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>Mark</td>
-                    <td>90,00</td>
-                    <td><i class="bi bi-box-arrow-in-down text-danger"></td>
+                @foreach($costs->all() as $cost)
+                <tr class="align-baseline text-center">
+
+                    <td>{{$cost->description}}</td>
+                    <td>{{$cost->value}}</td>
+                    @if($cost->type == 'exit')
+                    <td><i class="bi bi-box-arrow-in-up text-danger"></td>
+                    @else
+                    <td><i class="bi bi-box-arrow-in-down text-success"></td>
+
+                    @endif
                     <td>
-                        <div>
-                            <i class="bi bi-trash3-fill m-2 text-danger"></i>
-                        </div>
+                            <form action="{{route('cost.destroy',$cost->id)}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button class=" btn btn-danger"> <i class="bi bi-trash3-fill m-2 text-light"></i>
+                                </button>
+
+                            </form>
                     </td>
 
 
 
                 </tr>
-                <tr>
-                    <td>Jacob</td>
-                    <td>150,00</td>
-                    <td><i class="bi bi-box-arrow-in-up text-success"></td>
-                    <td>
-                        <div>
-                            <i class="bi bi-trash3-fill m-2 text-danger"></i>
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Larry</td>
-                    <td>120,00</td>
+                @endforeach
 
-                    <td><i class="bi bi-box-arrow-in-up text-success"></td>
-                    <td>
-                        <div>
-                            <i class="bi bi-trash3-fill m-2 text-danger"></i>
-                        </div>
-                    </td>
-                </tr>
             </tbody>
         </table>
 
